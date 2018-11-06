e27bc10942e1d6062d003314b40ddbcc45a6cd7f
Mitchell Quigg <Mitchell.Quigg@iam.local>
2018-11-06 13:49:02 -0600

    MAde starter player controls script

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour {
 
+
+    public float maxSpeed = 10f;
+
+    bool facingright = true;
+
+    Rigidbody2D rigidbody2D;
+
+    Animator anim;
+
 	// Use this for initialization
 	void Start () {
-		
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate ()
+    {
+        float move = Input.GetAxis("Horizontal");
+
+        rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+
+        if (move > 0 && !facingright)
+            Flip();
+        else if(move < 0 && facingright)
+                Flip();
+
+
 	}
+
+    void Flip()
+    {
+        facingright = !facingright;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+    }
 }
