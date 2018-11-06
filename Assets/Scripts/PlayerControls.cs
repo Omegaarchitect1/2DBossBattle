a8ad7dccafe309110a1856ff3c10b61f035b66ec
Mitchell Quigg <Mitchell.Quigg@iam.local>
2018-11-06 14:13:36 -0600

    Player Controls complete!

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -13,16 +13,36 @@ public class PlayerControls : MonoBehaviour {
 
     Animator anim;
 
+    bool grounded = false;
+
+    public Transform groundCheck;
+
+    float groundRadius = 0.2f;
+
+    public LayerMask whatIsGround;
+
+    public float jumpForce = 700;
+
 	// Use this for initialization
 	void Start () {
         rigidbody2D = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
 	}
 	
+	
 	// Update is called once per frame
 	void FixedUpdate ()
     {
+       
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("Ground", grounded);
+
+        anim.SetFloat("vSpeed", rigidbody2D.velocity.y);
+
+
         float move = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("Speed", Mathf.Abs(move));
 
         rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
 
@@ -33,6 +53,15 @@ public class PlayerControls : MonoBehaviour {
 
 
 	}
+
+    private void Update()
+    {
+        if(grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("Ground", false);
+            rigidbody2D.AddForce(new Vector2(0, jumpForce));
+        }
+    }
 
     void Flip()
     {
