136aa3c76cfc65fed07582d12044e70042e36c46
Mitchell Quigg <Mitchell.Quigg@iam.local>
2018-12-11 15:16:53 -0600

    re-adding scripts

---
+++ Scripts/Hazard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Hazard : MonoBehaviour {
+
+    private AudioSource audiosource;
+
+    private void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+            audiosource.Play();
+            PlayerControls player = collision.GetComponent<PlayerControls>();
+            player.isDead = true;
+            player.anim.SetBool("isDead", player.isDead);
+           // player.Respawn();
+            //player.isDead = false;
+           // player.anim.SetBool("isDead", player.isDead);
+
+        }
+    }
+}
